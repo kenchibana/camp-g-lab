fdf563145f3d42cd6de4c04ced7ed34b470e7eb1
kenchibana <goldexperience.requiem03144030@gmail.com>
2020-10-01 06:58:14 +0000

    マイグレーションファイル変更

--- db/migrate/20200930111520_create_faivorites.rb
+++ db/migrate/20200930111520_create_faivorites.rb
@@ -1,7 +1,8 @@
 class CreateFaivorites < ActiveRecord::Migration[5.2]
   def change
     create_table :faivorites do |t|
-
+      t.integer :user_id
+      t.integer :review_id
       t.timestamps
     end
   end
--- db/migrate/20200930111555_create_reviews.rb
+++ db/migrate/20200930111555_create_reviews.rb
@@ -1,7 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
-
+      t.integer :user_id
+      t.string :contentable_id
+      t.dtring :contentable_type
       t.timestamps
     end
   end
